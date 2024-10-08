cdc5fcea2c48b6a2fa7cee8489dcc1110f8e113e
Matovu Ronald <maronwhite6@gmail.com>
2024-10-08 21:00:15 +0300

    refactor: add HuggingFace token configuration

--- config/services.php
+++ config/services.php
@@ -30,4 +30,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    // other services
+    'huggingface' => [
+        'token' => env('HUGGING_FACE_TOKEN'),
+    ],
+
 ];
