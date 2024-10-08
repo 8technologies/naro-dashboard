04fbf95fd84b8772d037450623a9d9fea5fde695
Matovu Ronald <maronwhite6@gmail.com>
2024-10-08 21:00:32 +0300

    Add 'responded_by' and 'expert_id' columns to messages table

--- database/migrations/2024_10_08_092228_create_messages_table.php
+++ database/migrations/2024_10_08_092228_create_messages_table.php
@@ -17,8 +17,11 @@ class CreateMessagesTable extends Migration
             $table->id();
             $table->foreignId('conversation_id')->constrained('conversations')->onDelete('cascade');
             $table->enum('sender', ['farmer', 'system']);
+            $table->enum('responded_by', ['system', 'expert'])->nullable();
+            $table->foreignId('expert_id')->nullable()->constrained('users')->onDelete('cascade'); // Nullable foreign key for expert (if any)
             $table->text('message');
             $table->enum('message_type', ['text', 'image', 'video'])->default('text');
+
             $table->timestamps();
         });
     }
